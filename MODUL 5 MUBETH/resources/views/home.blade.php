4a5c64404f319ebb28f82f7656473008484b68b9
Mubeth Praditya <95601358+MubethHashmicro@users.noreply.github.com>
2021-12-11 15:15:43 +0700

    first commit M5

---
+++ MODUL 5 MUBETH/resources/views/home.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <!-- jQuery and JS bundle w/ Popper.js -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- FONT AWESOME -->
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- Datepicker CSS -->
+    <link rel="stylesheet" href="css/datepicker.css">
+
+    <!-- Datepicker JS-->
+    <script src="js/datepicker.js"></script>
+
+    <title>EAD TRAVEl</title>
+</head>
+<body>
+<!-- NAVBAR -->
+<body>
+<nav class="navbar navbar-light navbar-expand-md bg-light justify-content-md-center justify-content-start">
+        <a class="navbar-brand d-md-none d-inline" href="">Brand</a>
+        <button class="navbar-toggler ml-1" type="button" data-toggle="collapse" data-target="#collapsingNavbar2">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-collapse collapse justify-content-between align-items-center w-100" id="collapsingNavbar2">
+            <ul class="navbar-nav mx-auto text-md-center text-left">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">HOME</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/vaccine">VACCINE</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">PATIENT</a>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="d-flex justify-content-center my-3">
+            <h2>About Us</h2>
+        </div>
+<br><br><br><br>
+        <div class="row">
+            <img src="https://img.freepik.com/free-vector/health-professional-team_52683-36023.jpg?size=626&ext=jpg" alt=" " height="300px">
+            <div class="col-md-6"><br><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque veritatis reiciendis soluta accusantium? Consectetur at sapiente accusamus voluptatem, dignissimos explicabo quas commodi voluptate cumque recusandae dicta quos voluptatum doloremque expedita rerum, aliquid omnis rem cupiditate totam nihil maiores ipsum nisi quod. Porro cupiditate amet </div>
+        </div>
+    </div>
+</body>
---
+++ MODUL 5 MUBETH/resources/views/vaccine.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <!-- jQuery and JS bundle w/ Popper.js -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- FONT AWESOME -->
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- Datepicker CSS -->
+    <link rel="stylesheet" href="css/datepicker.css">
+
+    <!-- Datepicker JS-->
+    <script src="js/datepicker.js"></script>
+
+    <title>EAD TRAVEl</title>
+</head>
+<body>
+<!-- NAVBAR -->
+<body>
+<nav class="navbar navbar-light navbar-expand-md bg-light justify-content-md-center justify-content-start">
+        <a class="navbar-brand d-md-none d-inline" href="">Brand</a>
+        <button class="navbar-toggler ml-1" type="button" data-toggle="collapse" data-target="#collapsingNavbar2">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-collapse collapse justify-content-between align-items-center w-100" id="collapsingNavbar2">
+            <ul class="navbar-nav mx-auto text-md-center text-left">
+                <li class="nav-item">
+                    <a class="nav-link" href="/home">HOME</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">VACCINE</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">PATIENT</a>
+            </ul>
+        </div>
+    </nav>
+
+
+</body>
